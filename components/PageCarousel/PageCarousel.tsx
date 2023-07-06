f79ce703fb3a7d3a51f44ec025b55bcd2f0e3ae2
Vyacheslav <drimstor@gmail.com>
2023-07-06 22:50:56 +0300

    mobile version

--- components/PageCarousel/PageCarousel.tsx
+++ components/PageCarousel/PageCarousel.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, WheelEvent } from 'react'
+import React, { TouchEvent, useEffect, useState, WheelEvent } from 'react'
 import styles from './PageCarousel.module.scss'
 import FirstScreen from 'components/FirstScreen/FirstScreen'
 import Planet from 'components/Planet/Planet'
@@ -9,6 +9,7 @@ import useDebounce from 'hooks/useDebounce'
 import useDebounceCallback from 'hooks/useDebounceCallback'
 
 const PageCarousel = () => {
+  // ------------- Desktop ------------ //
   const [scrollToDirection, setScrollToDirection] = useState<number>(0)
   const [screenNumber, setScreenNumber] = useState('0')
   const [isScrollLock, setIsScrollLock] = useState(false)
@@ -17,18 +18,68 @@ const PageCarousel = () => {
     setIsScrollLock(false)
   }
 
+  const scrollToNewScreen = (isNext = true) => {
+    setScrollToDirection(isNext ? 1 : -1)
+    setTimeout(() => {
+      setIsScrollLock(true)
+      setScrollToDirection(0)
+    }, 100)
+  }
+
   const onPageWheelHandler = (e: WheelEvent<HTMLElement>) => {
     if (!isScrollLock) {
       if (e.nativeEvent.deltaY > 0) {
-        setScrollToDirection(1)
+        scrollToNewScreen()
       } else {
-        setScrollToDirection(-1)
+        scrollToNewScreen(false)
       }
+    }
+  }
 
-      setTimeout(() => {
-        setIsScrollLock(true)
-        setScrollToDirection(0)
-      }, 100)
+  // ------------- Mobile ------------ //
+  const [isDragging, setIsDragging] = useState(false)
+  const [startPosition, setStartPosition] = useState({ clientX: 0, clientY: 0 })
+  const [currentPosition, setCurrentPosition] = useState({
+    clientX: 0,
+    clientY: 0,
+  })
+  const [translateDirection, setTranslateDirection] = useState(false)
+
+  const onMouseDownHandler = (e: TouchEvent<HTMLDivElement>) => {
+    setIsDragging(true)
+    setStartPosition({
+      clientX: e.targetTouches[0].clientX,
+      clientY: e.targetTouches[0].clientY,
+    })
+  }
+
+  const onMouseMoveHandler = (e: TouchEvent<HTMLDivElement>) => {
+    if (isDragging) {
+      const currentPositionX = e.targetTouches[0].clientX
+      const currentPositionY = e.targetTouches[0].clientY
+      setCurrentPosition({
+        clientX: currentPositionX,
+        clientY: currentPositionY,
+      })
+      setTranslateDirection(startPosition.clientY - currentPositionY >= 0)
+    }
+  }
+
+  const onMouseUpHandler = (e: TouchEvent<HTMLDivElement>) => {
+    setIsDragging(false)
+    if (
+      Math.round(e.changedTouches[0].pageY) !==
+        Math.round(startPosition.clientY) &&
+      Math.round(e.changedTouches[0].pageX) !==
+        Math.round(startPosition.clientX) &&
+      currentPosition.clientX - startPosition.clientX < 50 &&
+      startPosition.clientX - currentPosition.clientX < 50
+    ) {
+      if (translateDirection) {
+        scrollToNewScreen()
+      } else {
+        scrollToNewScreen(false)
+      }
     }
   }
 
@@ -51,27 +102,27 @@ const PageCarousel = () => {
         }
         if (screenNumber === '1') {
           setScreenNumber('2_1')
-          setTimeout(clearScrollLock, 1700)
+          setTimeout(clearScrollLock, 1500)
         }
         if (screenNumber === '2_1') {
           setScreenNumber('2_2')
-          setTimeout(clearScrollLock, 1700)
+          setTimeout(clearScrollLock, 1500)
         }
         if (screenNumber === '2_2') {
           setScreenNumber('2_3')
-          setTimeout(clearScrollLock, 1700)
+          setTimeout(clearScrollLock, 1500)
         }
         if (screenNumber === '2_3') {
           setScreenNumber('2_4')
-          setTimeout(clearScrollLock, 1700)
+          setTimeout(clearScrollLock, 1500)
         }
         if (screenNumber === '2_4') {
           setScreenNumber('2_5')
-          setTimeout(clearScrollLock, 1700)
+          setTimeout(clearScrollLock, 1500)
         }
         if (screenNumber === '2_5') {
           setScreenNumber('2_6')
-          setTimeout(clearScrollLock, 1700)
+          setTimeout(clearScrollLock, 1500)
         }
       }
       if (scrollToDirection < 0) {
@@ -91,23 +142,23 @@ const PageCarousel = () => {
 
         if (screenNumber === '2_2') {
           setScreenNumber('2_1')
-          setTimeout(clearScrollLock, 1700)
+          setTimeout(clearScrollLock, 1300)
         }
         if (screenNumber === '2_3') {
           setScreenNumber('2_2')
-          setTimeout(clearScrollLock, 1700)
+          setTimeout(clearScrollLock, 1500)
         }
         if (screenNumber === '2_4') {
           setScreenNumber('2_3')
-          setTimeout(clearScrollLock, 1700)
+          setTimeout(clearScrollLock, 1500)
         }
         if (screenNumber === '2_5') {
           setScreenNumber('2_4')
-          setTimeout(clearScrollLock, 1700)
+          setTimeout(clearScrollLock, 1500)
         }
         if (screenNumber === '2_6') {
           setScreenNumber('2_5')
-          setTimeout(clearScrollLock, 1700)
+          setTimeout(clearScrollLock, 1500)
         }
       }
     }
@@ -119,6 +170,9 @@ const PageCarousel = () => {
       <section
         className={clsx(styles.globalBox, styles['screen' + screenNumber])}
         onWheel={onPageWheelHandler}
+        onTouchStart={onMouseDownHandler}
+        onTouchMove={onMouseMoveHandler}
+        onTouchEnd={onMouseUpHandler}
       >
         <FirstScreen />
         <SecondScreen isActiveScreen={screenNumber === '1'} />
---
+++ components/ThirdScreen/constants.ts
@@ -0,0 +1,45 @@
+export const animateFramesConfig: animateFramesConfigType = {
+  '2_1': {
+    title: 'Гарантии',
+    text1: 'Гарантируем быструю обработку ваших',
+    text2: 'платежей, безопасность транзакций и высокую',
+    text3: 'конвертацию из заявки в успешную оплату',
+    reverse: false,
+  },
+  '2_2': {
+    title: 'Оплата банковским переводом',
+    text1: 'Под каждую заявку вашего клиента мы',
+    text2: 'подберем и выдадим реквизит нужного банка',
+    reverse: true,
+  },
+  '2_3': {
+    title: 'Электронные кошельки',
+    text1: 'Возможность оплат товаров и услуг при помощи',
+    text2: 'электронных кошельков - Qiwi, YooMoney',
+    reverse: false,
+  },
+  '2_4': {
+    title: 'Платежи на сим-карты',
+    text1: 'Самый простой способ оплаты для клиента - ',
+    text2: 'пополнение мобильного счета',
+    reverse: true,
+  },
+  '2_5': {
+    title: 'Альтернативные возможности',
+    text1: 'Мы можем подобрать индивидуальное',
+    text2: 'оптимальное решение под любой бизнес',
+    reverse: false,
+    button: true,
+  },
+}
+
+interface animateFramesConfigType {
+  [key: string]: {
+    title: string
+    text1: string
+    text2: string
+    text3?: string
+    reverse: boolean
+    button?: boolean
+  }
+}
